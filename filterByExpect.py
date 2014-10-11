23f4b5644b4c155bc6461732ebfdfa6fe8068a85
Shirish Goyal <shirish.goyal@gmail.com>
2014-10-11 15:45:37 +0530

    search Accession by Phage and Cluster

--- filterByExpect.py
+++ filterByExpect.py
@@ -1,6 +1,7 @@
 import time
 from parsers.phage_file import PhageDBReader
 
+
 if __name__ == '__main__':
     current_milli_time = lambda: int(round(time.time() * 1000))
     parser = PhageDBReader('data/blast-phagesdb.txt')
---
+++ getAccession.py
@@ -0,0 +1,10 @@
+import time
+from parsers.find_accession import PhageFinder
+
+
+if __name__ == '__main__':
+    current_milli_time = lambda: int(round(time.time() * 1000))
+    finder = PhageFinder('data/PhagesDB_Data.txt')
+
+    print finder.findByPhage('Arbiter', 'B2')
+    print finder.findByPhage('Ariel', 'J')
