dae325d9d9f716a915971a50355f1075d93ca29c
mbonsma <madeleine.bonsma@gmail.com>
2015-03-08 14:52:06 -0400

    Update filterByExpectPhages.py
    Changed file saving to rename extension properly.

--- filterByExpectPhages.py
+++ filterByExpectPhages.py
@@ -3,11 +3,13 @@
 import time, os
 from parsers.phage_file import PhageDBReader
 
-
 for fn in os.listdir("data/phages"):
+    ext = fn.index('.')
+    outfile = fn.replace(fn[ext+1:],("csv"))
+    
     if __name__ == '__main__':
         current_milli_time = lambda: int(round(time.time() * 1000))
         parser = PhageDBReader('data/'+'phages/'+fn)
         # parser.write('output/blast-phagesdb.%s.csv' % current_milli_time())
 
-        parser.filterByExpect('output/%s.csv' % fn)
+        parser.filterByExpect('output/%s' % outfile)
