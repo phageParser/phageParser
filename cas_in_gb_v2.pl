aea6e14986ba653dd6b81a7bff2fec606548d71d
JJ Merelo <jjmerelo@gmail.com>
2015-06-05 19:09:59 +0200

    Detecting seq descriptions and outputting type
    This is the output right now
    
        -> 229..1590 ==> Type I
    
    Let me know if this is what you are looking for @mbonsma and what format do you want the output refs goyalsid/phageParser#36

--- cas_in_gb_v2.pl
+++ cas_in_gb_v2.pl
@@ -11,6 +11,11 @@ my $gb_file = shift || "data/Genbank_example.txt";
 
 my $file_contents = read_file( $gb_file);
 
+my %cas = ( Cas3 => 'Type I',
+	    Cas9 => 'Type II', 
+	    Cas10 => 'Type III');
+my $cas_str = join("|",keys %cas );
+my $cas_regex = qr/($cas_str)/;
 
 my @loci = split(/LOCUS\s+/s, $file_contents );
 
@@ -21,6 +26,11 @@ for my $l (@loci ) {
     next if $g !~ m{/product};
     my ($seq ) = ($g =~ /(\d+\.\.\d+)/);
     my (@products) = ($l =~ m{/product="([^\"]+)"}g);
-    say "-> $seq ==> ", join(" - ", @products );
+    for my $p (@products) {
+	if ( $p =~ $cas_regex ) {
+	    my $pattern = $1;
+	    say "-> $seq ==> ", $cas{$pattern};
+	  }
+      }
   }
 }
