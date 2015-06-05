11af971f691472f7892af473fca229b3976744ff
JJ Merelo <jjmerelo@gmail.com>
2015-06-05 18:10:15 +0200

    Please check the program @daisieh and #mbonsma
    Right now it splits into loci, and then into genes, and then it prints out the numbers and the possible products. Only there does not seem to be any Cas3, 6 or 9. Refs #36

--- cas_in_gb_v2.pl
+++ cas_in_gb_v2.pl
@@ -16,6 +16,11 @@ my @loci = split(/LOCUS\s+/s, $file_contents );
 
 for my $l (@loci ) {
   next if ! ($l =~ m{/product});
-  my (@products) = ($l =~ m{/product="([^\"]+)"}g);
-  say @product;
+  my ( @genes ) = split(/\n\s+gene\s{2,}/, $l );
+  for my $g ( @genes ) {
+    next if $g !~ m{/product};
+    my ($seq ) = ($g =~ /(\d+\.\.\d+)/);
+    my (@products) = ($l =~ m{/product="([^\"]+)"}g);
+    say "-> $seq ==> ", join(" - ", @products );
+  }
 }
