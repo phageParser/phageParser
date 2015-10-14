9ec5a35775c32e3754477005c92537a3d9d36821
Nicola Rizzo <nicola.n.rizzo@gmail.com>
2015-10-14 21:26:15 +0200

    Add stop/restart feature and refactor the code.

--- extract_CRISPRdb.py
+++ extract_CRISPRdb.py
@@ -11,114 +11,103 @@ def get_dom(url):
     return dom
 
 def get_taxons_from_CRISPRdb():
+    taxon_ids = []
+    if os.path.exists('taxon_ids.csv'):
+        with open('taxon_ids.csv', 'r') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row in reader:
+                taxon_ids.append(row[0])
+            return taxon_ids
     url = "http://crispr.u-psud.fr/crispr/"
     dom_homepage = get_dom(url)
     container = dom_homepage('div[class="strainlist"]')[0]
-    crisprs = []
     for link in container('a'):
-        crispr = {}
-        crispr['Name'] = link.content.encode('ascii','ignore')
-        crispr['Taxon_id'] = link.href.encode('ascii','ignore')[46:]
-        crisprs.append(crispr)
-    return crisprs
+        taxon_id = link.href.encode('ascii','ignore')[46:]
+        taxon_ids.append(taxon_id)
+        with open('taxon_ids.csv', 'a') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([taxon_id])
+    return taxon_ids
 
-def get_genome_properties(crispr):
-    url = "http://crispr.u-psud.fr/cgi-bin/crispr/SpecieProperties.cgi?Taxon_id=" + crispr['Taxon_id']
+def get_sequences(taxon_id):
+    url = "http://crispr.u-psud.fr/cgi-bin/crispr/SpecieProperties.cgi?Taxon_id=" + taxon_id
     dom = get_dom(url)
     table = dom('table[class="primary_table"]')[1]
-    crispr['Ref_seqs']={}
+    sequences = []
     for sequence in table('tr'):
+        seq = []
         if sequence('th')==[]:
-            crispr['Ref_seqs'][sequence('td')[1].content.encode('ascii','ignore')]=[]
-    return crispr
+            seq = {}
+            seq['Taxon_id'] = taxon_id
+            seq['RefSeq'] = sequence('td')[1].content.encode('ascii','ignore')
+            sequences.append(seq)
+    return sequences
 
-def get_CRISPR_properties(crispr):
-    for ref_seq in crispr['Ref_seqs'].keys():
-        url = "http://crispr.u-psud.fr/crispr/CRISPRProperties.php?RefSeq=" + ref_seq + "&Taxon=" + crispr['Taxon_id']
-        dom = get_dom(url)
-        div = dom('div[class="rightcontent"]')[1]
-        table = div('table[class="primary_table"]')[0]
-        tbody = table('tbody')[0]
-        for crispr_id in tbody('tr'):
-            cell = crispr_id('td')[1]
-            crispr_id_value = cell('font')[0].content.encode('ascii','ignore').replace('<br/n/>','')
-            crispr_id_value = crispr_id_value.replace('\n','')
-            crispr_id_value = crispr_id_value.replace('\t','')
-            crispr_id_value = crispr_id_value.replace('<br />','')
-            crispr['Ref_seqs'][ref_seq].append(crispr_id_value)
-    return crispr
+def get_loc_ids(sequence):
+    url = "http://crispr.u-psud.fr/crispr/CRISPRProperties.php?RefSeq=" + sequence['RefSeq']
+    dom = get_dom(url)
+    div = dom('div[class="rightcontent"]')[1]
+    table = div('table[class="primary_table"]')[0]
+    tbody = table('tbody')[0]
+    loc_ids = []
+    for crispr_id in tbody('tr'):
+        cell = crispr_id('td')[1]
+        crispr_id_value = cell('font')[0].content.encode('ascii','ignore').replace('<br/n/>','')
+        crispr_id_value = crispr_id_value.replace('\n','')
+        crispr_id_value = crispr_id_value.replace('\t','')
+        crispr_id_value = crispr_id_value.replace('<br />','')
+        loc_ids.append(crispr_id_value.split('_')[-1])
+    return loc_ids
 
-def get_begin(source):
-    begin = re.search('(?<=Crispr_begin_position: )\d+', source)
-    begin = begin.group(0)
-    return begin
-
-def get_end(source):
-    end = re.search('(?<=Crispr_end_position: )\d+', source)
-    end = end.group(0)
-    return end
-
-def get_loc_id(source):
-    loc_id = re.search('(?<=Crispr Rank in the sequence: )\d+', source)
-    loc_id = loc_id.group(0)
-    return loc_id
-
-def download(source,a,b):
-    directory = "files/"
-    namefile =  a + "_" + b
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    f = open(directory + namefile, 'w')
-    f.write(source)
-    f.close()
+def get_positions(sequence,loc_id):
+    params = {}
+    params['Taxon'] =  sequence['Taxon_id']
+    crispr_id = sequence['RefSeq'] + "_" + loc_id
+    params['checked[]'] = crispr_id
+    r = requests.post("http://crispr.u-psud.fr/crispr/crispr.php", data=params)
+    source = web.Element(r.text)
+    print "Crispr id: " + crispr_id
+    table = source('table[class="crispr"]')[0]
+    tr = table('tr')[3]
+    td = tr('td')
+    begin = re.search('(?<=</span>) +\d+', td[0].content)
+    begin = begin.group(0).split(' ')[-1]
+    end = re.search('(?<=</span>) +\d+', td[1].content)
+    end = end.group(0).split(' ')[-1]
+    return [begin,end]
 
 def get_results():
-    crisprs =  get_taxons_from_CRISPRdb()
-    n = len(crisprs)
+    print "Getting taxon ids..."
+    taxon_ids =  get_taxons_from_CRISPRdb()
+    n = len(taxon_ids)
     i = 0.
-    print "Getting genome and CRISPR properties..."
-    start = time.time()
-    for crispr in crisprs:
+    print "Getting positions..."
+    restart = False
+    var = False
+    if os.path.exists('results.csv'):
+        f = open('last','r')
+        last = f.read()
+        [last_taxon_id,last_refseq,last_loc_id] = last.split(',')
+        restart = True
+    for taxon_id in taxon_ids:
         print "{:.2%}".format(i/n)
-        crispr = get_genome_properties(crispr)
-        crispr = get_CRISPR_properties(crispr)
+        print "Taxon id: " + taxon_id
+        if(restart == True and last_taxon_id == taxon_id):
+            var = True
+            continue
+        if (var == True or restart == False):
+            sequences = get_sequences(taxon_id)
+            for sequence in sequences:
+                loc_ids = get_loc_ids(sequence)
+                sequence['Loc_ids'] = loc_ids
+                for loc_id in sequence['Loc_ids']:
+                    [begin,end] = get_positions(sequence,loc_id)
+                    result = [sequence['RefSeq'],loc_id,begin,end]
+                    with open('results.csv', 'a') as csvfile:
+                        writer = csv.writer(csvfile, delimiter=',')
+                        writer.writerow(result)
+                    f = open('last','w')
+                    f.write(sequence['Taxon_id'] + "," + sequence['RefSeq'] + "," + loc_id)
         i += 1
-    end = time.time()
-    print "Getting properties required " + str(end-start) + " seconds."
-    results = []
-    i = 0.
-    print "Downloading files..."
-    start = time.time()
-    for crispr in crisprs:
-        print "{:.2%}".format(i/n)
-        for ref_seq in crispr['Ref_seqs'].keys():
-            for crispr_id in crispr['Ref_seqs'][ref_seq]:
-                params = {'checked[]': crispr_id, 'Taxon': crispr['Taxon_id']}
-                r = requests.post("http://crispr.u-psud.fr/crispr/crispr.php", data=params)
-                file_crispr_seq = web.Element(r.text)
-                table = file_crispr_seq('table[class="crisprs_table"]')[0]
-                url = "http://crispr.u-psud.fr" + table('form')[-2].action
-                source = requests.get(url).text
-                begin = get_begin(source)
-                end = get_end(source)
-                main_accession_number = ref_seq
-                loc_id = get_loc_id(source)
-                download(source,main_accession_number,loc_id)
-                results.append([main_accession_number,loc_id,begin,end])
-        i += 1
-    end = time.time()
-    print "Downloading files required " + str(end-start) + " seconds."
 
-    print "Writing .csv file..."
-    start = time.time()
-    with open('results.csv', 'wb') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        for result in results:
-            writer.writerow(result)
-    end = time.time()
-    print "Writing .csv file required " + str(end-start) + " seconds."
-
-start = time.time() 
 get_results()
-end = time.time()
-print "The complete process required " + str(end-start) + " seconds."
