2ef9f5a4e70b5e5fb54983590ac3c14b6dbe9fca
Nigesh Shakya <nizeshshakya@gmail.com>
2016-06-07 20:17:43 -0500

    patch for issue 3 -> Genome Extractor

---
+++ parsers/genome_extractor.py
@@ -0,0 +1,42 @@
+import re
+import csv
+from csv_file import CSVWriter, CSVReader
+
+''' 
+@author:         Nigesh Shakya(neaGaze)
+@timestamp:      06/08/2016 @ 12:54am (UTC)
+    
+The Class file that examines the file downloaded via prefetch (see #2), and returns a string corresponding to the genome starting n bases before startPosition and carrying on to m bases after endPosition. If either n or m are not provided, default them to 100.
+This patch pertains to issue #3 in the github wiki (https://github.com/goyalsid/phageParser/issues/3)
+'''
+class GenomeExtractor:
+
+    def __init__(self, infile, **kwargs):
+        blast_file = open(infile, 'r')
+        self.reader = CSVReader(blast_file, dialect=csv.excel_tab)
+        pass
+
+    '''  Genome Extractor '''
+    def findNeighbours(self, startPosition, endPosition, n=100, m=100):
+        count = 0
+        hasDataFound = False
+        matchedGene = ""
+        for row in self.reader:
+
+            if row[0] == "//":
+                break
+
+            if row[0].startswith("ORIGIN"): 
+                hasDataFound = True
+                continue
+
+            if hasDataFound:
+                rowVals = row[0].split()
+                pos = int(rowVals[0])
+                if pos >= startPosition -n and pos < endPosition + m:
+                    matchedGene += " ".join(rowVals)
+                    matchedGene += "\n"
+                     
+        return matchedGene
+
+
