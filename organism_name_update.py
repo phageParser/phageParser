2b20a32dcdb00d18232c302f1c91b5db32c60784
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2017-06-01 11:17:30 -0400

    Remove unnecessary .keys() calls.

--- organism_name_update.py
+++ organism_name_update.py
@@ -65,7 +65,7 @@ def fetch_names(id_list):
     return organism_names
 
 def insert_names(organism_names):
-	for key in organism_names.keys():
+	for key in organism_names:
 		c.execute('UPDATE Organism SET OrganismName=? WHERE Accession=?', [organism_names[key], key])
         print(key + ': ' + organism_names[key])
 
