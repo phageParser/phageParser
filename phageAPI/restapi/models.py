f72904a6369075083c76aba1bc0d377fb5aeeb09
Cem Yildirim <cemyildirim@fastmail.com>
2017-06-05 10:54:58 -0400

    Modifies the field constraints and changes evalue to float.

--- phageAPI/restapi/models.py
+++ phageAPI/restapi/models.py
@@ -4,40 +4,40 @@ from django.db import models
 
 
 class Spacer(models.Model):
-    sequence = models.CharField(max_length=2000, blank=True, default='')
+    sequence = models.CharField(max_length=255, blank=True, default='')
 
 
 class Repeat(models.Model):
-    sequence = models.CharField(max_length=2000, blank=True, default='')
+    sequence = models.CharField(max_length=255, blank=True, default='')
 
 
 class AntiCRISPR(models.Model):
-    sequence = models.CharField(max_length=2000, blank=True, default='')
-    accession = models.CharField(max_length=50, blank=True, default='')
+    sequence = models.CharField(max_length=255, blank=True, default='')
+    accession = models.CharField(max_length=32, blank=True, default='')
 
 
 class Organism(models.Model):
-    name = models.CharField(max_length=50, blank=True, default='')
-    accession = models.CharField(max_length=50, blank=True, default='')
+    name = models.CharField(max_length=512, blank=True, default='')
+    accession = models.CharField(max_length=32, blank=True, default='')
 
     def __str__(self):
         return self.name
 
 
 class Phage(models.Model):
-    accession = models.CharField(max_length=50, blank=True, default='')
+    accession = models.CharField(max_length=32, blank=True, default='')
 
 
 class CasProtein(models.Model):
-    profileID = models.CharField(max_length=50, blank=True, default='')
-    function = models.CharField(max_length=50, blank=True, default='')
-    gene = models.CharField(max_length=50, blank=True, default='')
-    group = models.CharField(max_length=50, blank=True, default='')
-    type_specificity = models.CharField(max_length=50, blank=True, default='')
+    profileID = models.CharField(max_length=32, blank=True, default='')
+    function = models.CharField(max_length=32, blank=True, default='')
+    gene = models.CharField(max_length=32, blank=True, default='')
+    group = models.CharField(max_length=32, blank=True, default='')
+    type_specificity = models.CharField(max_length=255, blank=True, default='')
 
 
 class OrganismSpacerRepeatPair(models.Model):
-    # Has 1 organism and a spacerrepeatpair list with start and end values for
+    # Has 1 organism and a spacerrepeatpair list with start and end values in
     # locus
     organism = models.ForeignKey(Organism, on_delete=models.CASCADE, null=True)
     spacer = models.ForeignKey(Spacer, on_delete=models.CASCADE, null=True)
@@ -77,4 +77,4 @@ class OrganismCasPair(models.Model):
         CasProtein, on_delete=models.CASCADE, null=True)
     genomic_start = models.PositiveIntegerField(default=0)
     genomic_end = models.PositiveIntegerField(default=0)
-    evalue = models.PositiveIntegerField(default=0)
+    evalue = models.FloatField(default=0)
